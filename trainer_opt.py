96fda52d39c7f407f64fac038307e0557c902f92
George Malak <georgmalak2004@gmail.com>
2026-04-18 21:14:25 +0200

    Intialize Optimizers

---
+++ trainer_opt.py
@@ -0,0 +1,45 @@
+from PIL.Image import v
+
+import torch as th
+import torch.optim as opt
+from torch.optim import lr_scheduler
+
+class TrainerOpt:
+    def __init__(self,model , optimizer='adam',lr=0.001,scheduler_name=None):
+        self.model = model
+        self.device = th.device('cuda' if th.cuda.is_available() else 'cpu')
+        self.model.to(self.device)
+
+        if optimizer == 'adam':
+            self.optimizer = opt.Adam(self.model.parameters(), lr=lr)
+        elif optimizer == 'sgd':
+            self.optimizer = opt.SGD(self.model.parameters(), lr=lr , momentum=0.9)
+        elif optimizer == 'rmsprop':
+            self.optimizer = opt.RMSprop(self.model.parameters(), lr=lr)
+        elif optimizer == 'adagrad':
+            self.optimizer = opt.Adagrad(self.model.parameters(), lr=lr)
+        
+        self.scheduler = None
+        if scheduler_name == 'step':
+            self.scheduler = lr_scheduler.StepLR(self.optimizer, step_size=10, gamma=0.1)
+        elif scheduler_name == 'plateau':
+            self.scheduler = lr_scheduler.ReduceLROnPlateau(self.optimizer, mode='min', factor=0.1, patience=5)
+    
+    def train_step(self,X_train,criterion):
+        self.model.train()
+        total_loss = 0
+
+        for img, lab in X_train:
+            img , lab = img.to(self.device), lab.to(self.device)
+
+            self.optimizer.zero_grad()
+            output = self.model(img)
+            loss = criterion(output, lab)
+            loss.backward()
+            self.optimizer.step()
+            total_loss += loss.item()
+        
+        avg_loss = (total_loss / len(X_train))
+        if self.scheduler and not isinstance(self.scheduler, lr_scheduler.ReduceLROnPlateau):
+            self.scheduler.step(val_loss=avg_loss)
+        return avg_loss
